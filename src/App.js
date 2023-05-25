fdf212b99748beb521db4ac2815c6c58d189ae64
Andrew Wladis <andrewwladis@gmail.com>
2023-05-25 09:56:52 -0400

    added bold to definitions

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="App">
       <div className='info'>
         <p className='info-text'>
-          Atomic Number - The number of protons in the nucleus of an atom. This determines where it goes on the periodic table and the chemical properties of an element.
+          <b>Atomic Number</b> - The number of protons in the nucleus of an atom. This determines where it goes on the periodic table and the chemical properties of an element.
         </p>
         <p className='info-text'>
-          Atomic Mass - The the mass of an atom of a chemical element.
+          <b>Atomic Mass</b> - The the mass of an atom of a chemical element.
         </p>
         <p className='info-text'>
-          Periodic Table - A table that organizes all currently discovered chemical elements in rows (named periods) and columns (named groups) ordered by their increasing atomic number.
+          <b>Periodic Table</b> - A table that organizes all currently discovered chemical elements in rows (named periods) and columns (named groups) ordered by their increasing atomic number.
         </p>
       </div>
       {elements.elements.map((info) => <Element data={info} />)}
