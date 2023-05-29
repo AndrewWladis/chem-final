befac3763e9d141db4878a3b3b241d938b467c80
Andrew Wladis <andrewwladis@gmail.com>
2023-05-29 12:32:18 -0400

    rephrased the terms

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <div className="App">
       <div className='info'>
         <p className='info-text'>
-          <b>Atomic Number</b> - The number of protons in the nucleus of an atom. This determines where it goes on the periodic table and the chemical properties of an element.
+          <b>Atomic Number</b> - The number of protons in the nucleus of an atom. This determines where it's position on the periodic table and their chemical properties.
         </p>
         <p className='info-text'>
           <b>Atomic Mass</b> - The the mass of an atom of a chemical element.
         </p>
         <p className='info-text'>
-          <b>Periodic Table</b> - A table that organizes all currently discovered chemical elements in rows (named periods) and columns (named groups) ordered by their increasing atomic number.
+          <b>Periodic Table</b> - A table that organizes all currently discovered elements in rows (named periods) and columns (named groups) in increasing order by their atomic number.
         </p>
       </div>
       {elements.elements.map((info) => <Element data={info} />)}
